74bc9eb7d5b013e5af4fedd2a85fdfb869fb6bcf
samuditha jayawardena <54886813+samuditha97@users.noreply.github.com>
2022-05-07 22:46:57 +0530

    created files

---
+++ Backend/ds_assignment/src/test/java/com/movie_reservation/ds_assignment/DsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie_reservation.ds_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
